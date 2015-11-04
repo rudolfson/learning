440beb0e395c88534603b5683812a4ae72db725d
lhe <lhe@cedros.com>
2015-11-04 13:49:35 +0100

    day exercises

--- 7lang7weeks/2.3/do_2.rb
+++ 7lang7weeks/2.3/do_2.rb
@@ -1,9 +1,17 @@
 class Tree
     attr_accessor :children, :node_name
 
-    def initialize(name, children=[])
-        @children = children
-        @node_name = name
+    def initialize(nameOrHash, children=[])
+        if nameOrHash.is_a? Hash then
+            nameOrHash.each { |name, children| initialize(name, children) }
+        elsif children.is_a? Array then
+            @children = children
+            @node_name = nameOrHash
+        elsif children.is_a? Hash then
+            @node_name = nameOrHash
+            @children = []
+            children.each { |name, childhash| @children << Tree.new(name, childhash) }
+        end
     end
 
     def visit_all(&block)
@@ -16,7 +24,7 @@ class Tree
     end
 end
 
-ruby_tree = Tree.new("Ruby", [Tree.new("Reia"), Tree.new("MaxRuby")])
+ruby_tree = Tree.new( { 'grandpa' => { 'dad' => { 'child 1' => {}, 'child 2' => {} }, 'uncle' => { 'child 3' => {}, 'child 4' => {} } } } )
 
 puts "Visiting a node"
 ruby_tree.visit {|node| puts node.node_name}
